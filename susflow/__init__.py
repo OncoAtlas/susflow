5978104e6775eab7403acb0c40aab498a3e0496f
JoaoBoscoDuarte <joaoboscoduartef@gmail.com>
2026-05-29 02:16:03 -0300

    refactor(susflow): update public exports to renamed English identifiers

--- susflow/__init__.py
+++ susflow/__init__.py
@@ -1,3 +1,3 @@
 from .ftp import FileNotFoundOnFTPError, FTPError
 from .reader import ReadError, read
-from .systems import sim, sinan, sinasc
+from .systems import cnes, pni, siasus, sihsus, sim, sinan, sinasc
